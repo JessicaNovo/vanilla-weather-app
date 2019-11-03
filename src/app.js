dbdb0a5565bfadec05bb60b092b18b63f4b62216
JessicaNovo <50515278+JessicaNovo@users.noreply.github.com>
2019-11-03 01:19:36 +0000

    Updated weather icon from API

--- src/app.js
+++ src/app.js
@@ -23,12 +23,11 @@ function showCurrentDate() {
     "November",
     "December"
   ];
-
   let today = weekDays[now.getDay()];
   let month = months[now.getMonth()];
   let day = now.getDate();
-
   let date = document.querySelector("#date");
+
   date.innerHTML = `${today}, ${month} ${day}`;
 }
 
@@ -55,6 +54,9 @@ function displayWeather(response) {
   let maximumTemperature = document.querySelector("#maximum-temperature");
   let humidity = document.querySelector("#humidity");
   let wind = document.querySelector("#wind");
+  let weatherIcon = document.querySelector("#icon");
+  let iconCode = response.data.weather[0].icon;
+
   temperature.innerHTML = Math.round(response.data.main.temp);
   cityName.innerHTML = response.data.name;
   weatherDescription.innerHTML = response.data.weather[0].main;
@@ -62,6 +64,11 @@ function displayWeather(response) {
   maximumTemperature.innerHTML = Math.round(response.data.main.temp_max);
   humidity.innerHTML = Math.round(response.data.main.humidity);
   wind.innerHTML = Math.round(response.data.wind.speed);
+  weatherIcon.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${iconCode}@2x.png`
+  );
+  weatherIcon.setAttribute("alt", response.data.weather[0].main);
 }
 
 function convertSunHours(response) {
@@ -75,6 +82,7 @@ function convertSunHours(response) {
   let sunsetTime = new Date(sunsetUnix * 1000);
   let sunsetHour = sunsetTime.getHours();
   let sunsetMin = sunsetTime.getMinutes();
+
   if (sunriseHour < 10) {
     sunriseHour = `0${sunriseHour}`;
   }
@@ -87,9 +95,11 @@ function convertSunHours(response) {
   if (sunsetMin < 10) {
     sunsetMin = `0${sunsetMin}`;
   }
+
   sunrise.innerHTML = `${sunriseHour}:${sunriseMin}`;
   sunset.innerHTML = `${sunsetHour}:${sunsetMin}`;
 }
+
 let apiKey = "0438fc32e86f8783300a37cf62f26092";
 let city = "Póvoa de Varzim";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
